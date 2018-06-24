8e26e22580fba77f09b6daabc1470e8a371b1c7c
ryanoliversotto <ryan.sotto@outlook.com>
2018-06-24 16:44:42 -0500

    config complete

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,24 @@
-### Schema
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-CREATE DATABASE cat_db;
-USE cat_db;
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
-
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
