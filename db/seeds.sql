543db645b06a4bb477c5b9677a56655fd5a7eabc
ryanoliversotto <ryan.sotto@outlook.com>
2018-06-24 16:31:48 -0500

    DB Schema and Seeds and Config of MVC

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+### Seeds SQL
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Siracha Lamb and Lemongrass Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grass Fed Triple Patty Blue Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Keto Lettuce Wrapped Cheddar and Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('No Regular Bun Hamburger and Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pretzel Bun Panko Chicken and Bacon Burger', false);
