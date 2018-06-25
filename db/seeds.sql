31f42cc983589c033bdf4fc6a422da05b56c4252
ryanoliversotto <ryan.sotto@outlook.com>
2018-06-25 18:10:54 -0500

    Troubleshooting compatibility issue with Handlebars and Bootstrap

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,8 @@
-### Seeds SQL
+### Seeds Sql
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Siracha Lamb and Lemongrass Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Grass Fed Triple Patty Blue Cheese Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Keto Lettuce Wrapped Cheddar and Mushroom Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('No Regular Bun Hamburger and Swiss', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pretzel Bun Panko Chicken and Bacon Burger', false);
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Iceburg Wrapped Grass Fed Bacon Mushroom Swiss Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Hickory Smoked Chicken Thigh Siracha Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Ramen Bun Sweet Teriyaki Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Deep Fried Tempeh Bun Black Bean Vegan Burger');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Mini Sliders Cheddar and Beef Burger 6 Pack');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Tandori Fired Salmon Burger with Thai Basil and Lemon Grass');
